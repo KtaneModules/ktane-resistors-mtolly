fd3d25ed3388d2092a27863c8e9e81af8ea35f12
Michael Tolly <miketolly@gmail.com>
2016-09-11 14:52:25 -0500

    Get all needed bomb info

--- Assets/Examples/1.1 Module/ExampleModule.cs
+++ Assets/Examples/1.1 Module/ExampleModule.cs
@@ -1,9 +1,24 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
+using Newtonsoft.Json;
 
 public class ExampleModule : MonoBehaviour
 {
-    public KMSelectable[] buttons;
+    public KMSelectable[] pins;
     public GameObject[] wires;
+    public MeshRenderer[] bands;
+    public Material materialBlack;
+    public Material materialBrown;
+    public Material materialRed;
+    public Material materialOrange;
+    public Material materialYellow;
+    public Material materialGreen;
+    public Material materialBlue;
+    public Material materialViolet;
+    public Material materialGray;
+    public Material materialWhite;
+    public Material materialGold;
+    public Material materialSilver;
 
     int correctIndex;
     bool isActivated = false;
@@ -20,46 +35,124 @@ public class ExampleModule : MonoBehaviour
 
     void Init()
     {
-        for(int i = 0; i < buttons.Length; i++)
-        {
+        for(int i = 0; i < pins.Length; i++) {
             int j = i;
-            buttons[i].OnInteract += delegate () { OnPress(j); return false; };
+            pins[i].OnInteract += delegate () { OnPress(j); return false; };
         }
     }
 
     void ActivateModule()
     {
         isActivated = true;
+
+        KMBombInfo info = GetComponent<KMBombInfo> ();
+
+        string serial = JsonConvert.DeserializeObject<Dictionary<string, string>>(info.QueryWidgets (KMBombInfo.QUERYKEY_GET_SERIAL_NUMBER, null)[0])["serial"];
+        List<int> notDigitIndexes = new List<int> ();
+        for (int i = 0; i < serial.Length; i++) {
+            if (serial [i] < '0' || '9' < serial [i]) {
+                notDigitIndexes.Insert (0, i);
+            }
+        }
+        foreach (int i in notDigitIndexes) {
+            serial = serial.Remove (i, 1);
+        }
+        Debug.Log ("[Resistors] Serial number digits are " + serial);
+
+        int batteries = 0;
+        bool dcell = false;
+        foreach (string response in info.QueryWidgets (KMBombInfo.QUERYKEY_GET_BATTERIES, null)) {
+            int bats = JsonConvert.DeserializeObject<Dictionary<string, int>> (response)["numbatteries"];
+            batteries += bats;
+            if (bats == 1)
+                dcell = true;
+        }
+        Debug.Log ("[Resistors] " + batteries + " batteries, D cell = " + dcell);
+
+        bool litFRK = false;
+        foreach (string response in info.QueryWidgets(KMBombInfo.QUERYKEY_GET_INDICATOR, null)) {
+            Dictionary<string, string> obj = JsonConvert.DeserializeObject<Dictionary<string, string>> (response);
+            if (obj ["label"] == "FRK" && obj ["on"] == "True") {
+                litFRK = true;
+            }
+        }
+        Debug.Log ("[Resistors] Lit FRK = " + litFRK);
+
+        for (int i = 0; i < 10; i++) {
+            if (serial.Length <= i) {
+                bands [i].enabled = false;
+            } else {
+                switch (serial[i]) {
+                case '0':
+                    bands [i].material = materialBlack;
+                    break;
+                case '1':
+                    bands [i].material = materialBrown;
+                    break;
+                case '2':
+                    bands [i].material = materialRed;
+                    break;
+                case '3':
+                    bands [i].material = materialOrange;
+                    break;
+                case '4':
+                    bands [i].material = materialYellow;
+                    break;
+                case '5':
+                    bands [i].material = materialGreen;
+                    break;
+                case '6':
+                    bands [i].material = materialBlue;
+                    break;
+                case '7':
+                    bands [i].material = materialViolet;
+                    break;
+                case '8':
+                    bands [i].material = materialGray;
+                    break;
+                case '9':
+                    bands [i].material = materialWhite;
+                    break;
+                default:
+                    bands [i].enabled = false;
+                    break;
+                }
+            }
+        }
     }
 
-	void OnPress(int buttonNumber)
+    void OnPress(int buttonNumber)
     {
         GetComponent<KMAudio>().PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, transform);
 
         if (!isActivated) {
-            Debug.Log("Pressed button before module has been activated!");
+            Debug.Log("[Resistors] Pressed button before module has been activated!");
             GetComponent<KMBombModule>().HandleStrike();
-			return;
+            return;
         }
 
-		if (startedConnecting == -1) {
-			startedConnecting = buttonNumber;
-			return;
-		}
+        if (startedConnecting == -1) {
+            startedConnecting = buttonNumber;
+            pins [buttonNumber].GetComponentInChildren<MeshRenderer> ().material = materialWhite;
+            return;
+        }
 
-		SetWire (startedConnecting, buttonNumber, true);
+        SetWire (startedConnecting, buttonNumber, true);
         startedConnecting = -1;
     }
 
-	void SetWire(int point1, int point2, bool isPresent)
-	{
+    void SetWire(int point1, int point2, bool isPresent)
+    {
+        pins [point1].GetComponentInChildren<MeshRenderer> ().material = materialGold;
+        pins [point2].GetComponentInChildren<MeshRenderer> ().material = materialGold;
+
         if (point1 == point2) return;
-		connections [point1, point2] = isPresent;
-		connections [point2, point1] = isPresent;
+        connections [point1, point2] = isPresent;
+        connections [point2, point1] = isPresent;
 
-		// 0..7 are ABCDEFGH
+        // 0..7 are ABCDEFGH
 
-		int wireIndex = 0;
+        int wireIndex = 0;
         if (point1 > point2) {
             int temp = point1;
             point1 = point2;
@@ -101,6 +194,6 @@ public class ExampleModule : MonoBehaviour
         else if (point1 == 2 && point2 == 7) wireIndex = 26;
         else if (point1 == 3 && point2 == 7) wireIndex = 27;
         
-		wires [wireIndex].SetActive (isPresent);
-	}
+        wires [wireIndex].SetActive (isPresent);
+    }
 }
