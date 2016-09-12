e933a10a9cd25396fe8523a6b191c1c328544131
Michael Tolly <miketolly@gmail.com>
2016-09-11 21:23:30 -0500

    Clear and check buttons, almost working!

--- Assets/Examples/1.1 Module/ExampleModule.cs
+++ Assets/Examples/1.1 Module/ExampleModule.cs
@@ -19,40 +19,39 @@ public class ExampleModule : MonoBehaviour
     public Material materialWhite;
     public Material materialGold;
     public Material materialSilver;
+    public KMSelectable checkButton;
+    public KMSelectable clearButton;
 
     int correctIndex;
     bool isActivated = false;
     bool[,] connections;
-	int startedConnecting = -1;
-	double resistor1 = 0;
-	double resistor2 = 0;
-	double goalResistAB = double.PositiveInfinity;
-	double goalResistAC = double.PositiveInfinity;
-	double goalResistAD = double.PositiveInfinity;
-	double goalResistBC = double.PositiveInfinity;
-	double goalResistBD = double.PositiveInfinity;
-	// resistance C-D not checked
+    int startedConnecting = -1;
+    double resistor1 = 0;
+    double resistor2 = 0;
+    double goalResistAB = double.PositiveInfinity;
+    double goalResistAC = double.PositiveInfinity;
+    double goalResistAD = double.PositiveInfinity;
+    double goalResistBC = double.PositiveInfinity;
+    double goalResistBD = double.PositiveInfinity;
+    // resistance C-D not checked
 
     void Start()
-    {
-        Init();
-
-        GetComponent<KMBombModule>().OnActivate += ActivateModule;
-        connections = new bool[8, 8]; // all false
-    }
-
-    void Init()
     {
         for(int i = 0; i < pins.Length; i++) {
             int j = i;
             pins[i].OnInteract += delegate () { OnPress(j); return false; };
         }
+
+        GetComponent<KMBombModule>().OnActivate += ActivateModule;
+        checkButton.OnInteract += delegate () { OnCheck(); return false; };
+        clearButton.OnInteract += delegate () { OnClear(); return false; };
+        connections = new bool[8, 8]; // all false
     }
 
-	double DifferentFrom(double value)
-	{
-		return value * 2.0; // TODO randomize this
-	}
+    double DifferentFrom(double value)
+    {
+        return value * 2.0; // TODO randomize this
+    }
 
     void ActivateModule()
     {
@@ -92,9 +91,9 @@ public class ExampleModule : MonoBehaviour
         Debug.Log ("[Resistors] Lit FRK = " + litFRK);
 
         char firstDigit = serial.Length == 0 ? '0' : serial[0];
-		char lastDigit = serial.Length == 0 ? '0' : serial[serial.Length - 1];
-		bool primaryInputA = firstDigit == '0' || firstDigit == '2' || firstDigit == '4' || firstDigit == '6' || firstDigit == '8';
-		bool primaryOutputC = lastDigit == '0' || lastDigit == '2' || lastDigit == '4' || lastDigit == '6' || lastDigit == '8';
+        char lastDigit = serial.Length == 0 ? '0' : serial[serial.Length - 1];
+        bool primaryInputA = firstDigit == '0' || firstDigit == '2' || firstDigit == '4' || firstDigit == '6' || firstDigit == '8';
+        bool primaryOutputC = lastDigit == '0' || lastDigit == '2' || lastDigit == '4' || lastDigit == '6' || lastDigit == '8';
         int targetResistance;
         if (serial.Length == 0) {
             targetResistance = 0;
@@ -103,134 +102,156 @@ public class ExampleModule : MonoBehaviour
         } else {
             targetResistance = int.Parse(serial.Substring(0, 2));
         }
-		for (int i = 0; i < batteries; i++) {
-			targetResistance *= 10;
-		}
+        for (int i = 0; i < batteries; i++) {
+            targetResistance *= 10;
+        }
 
-		if (primaryInputA && primaryOutputC) {
-			goalResistAC = targetResistance;
-			if (litFRK) {
-				goalResistAD = targetResistance;
-			} else if (dcell) {
-				goalResistBD = 0;
-			}
-		} else if (primaryInputA && !primaryOutputC) {
-			goalResistAD = targetResistance;
-			if (litFRK) {
-				goalResistAC = targetResistance;
-			} else if (dcell) {
-				goalResistBC = 0;
-			}
-		} else if (!primaryInputA && primaryOutputC) {
-			goalResistBC = targetResistance;
-			if (litFRK) {
-				goalResistBD = targetResistance;
-			} else if (dcell) {
-				goalResistAD = 0;
-			}
-		} else if (!primaryInputA && !primaryOutputC) {
-			goalResistBD = targetResistance;
-			if (litFRK) {
-				goalResistBC = targetResistance;
-			} else if (dcell) {
-				goalResistAC = 0;
-			}
-		}
-		Debug.Log ("[Resistors] A to B resistance should be " + goalResistAB);
-		Debug.Log ("[Resistors] A to C resistance should be " + goalResistAC);
-		Debug.Log ("[Resistors] A to D resistance should be " + goalResistAD);
-		Debug.Log ("[Resistors] B to C resistance should be " + goalResistBC);
-		Debug.Log ("[Resistors] B to D resistance should be " + goalResistBD);
+        if (primaryInputA && primaryOutputC) {
+            goalResistAC = targetResistance;
+            if (litFRK) {
+                goalResistAD = targetResistance;
+            } else if (dcell) {
+                goalResistBD = 0;
+            }
+        } else if (primaryInputA && !primaryOutputC) {
+            goalResistAD = targetResistance;
+            if (litFRK) {
+                goalResistAC = targetResistance;
+            } else if (dcell) {
+                goalResistBC = 0;
+            }
+        } else if (!primaryInputA && primaryOutputC) {
+            goalResistBC = targetResistance;
+            if (litFRK) {
+                goalResistBD = targetResistance;
+            } else if (dcell) {
+                goalResistAD = 0;
+            }
+        } else if (!primaryInputA && !primaryOutputC) {
+            goalResistBD = targetResistance;
+            if (litFRK) {
+                goalResistBC = targetResistance;
+            } else if (dcell) {
+                goalResistAC = 0;
+            }
+        }
+        Debug.Log ("[Resistors] A to B resistance should be " + goalResistAB);
+        Debug.Log ("[Resistors] A to C resistance should be " + goalResistAC);
+        Debug.Log ("[Resistors] A to D resistance should be " + goalResistAD);
+        Debug.Log ("[Resistors] B to C resistance should be " + goalResistBC);
+        Debug.Log ("[Resistors] B to D resistance should be " + goalResistBD);
 
-		float puzzle = Random.value;
-		if (puzzle < 0.3f) {
-			// use resistor 1
-			resistor1 = targetResistance;
-			resistor2 = DifferentFrom (targetResistance);
-		} else if (puzzle < 0.6f) {
-			// use resistor 2
-			resistor2 = targetResistance;
-			resistor1 = DifferentFrom (targetResistance);
-		} else if (puzzle < 0.9f) {
-			// 2 resistors in serial
-			// TODO randomize this
-			resistor1 = targetResistance * 0.4;
-			resistor2 = targetResistance - resistor1;
-		} else {
-			// 2 resistors in parallel
-			// TODO randomize this
-			resistor1 = targetResistance * 1.5;
-			resistor2 = 1.0 / (1.0 / targetResistance - 1.0 / resistor1);
-		}
-		Debug.Log ("[Resistors] Top resistor is " + resistor1);
-		Debug.Log ("[Resistors] Bottom resistor is " + resistor2);
+        float puzzle = Random.value;
+        if (puzzle < 0.3f) {
+            // use resistor 1
+            resistor1 = targetResistance;
+            resistor2 = DifferentFrom (targetResistance);
+        } else if (puzzle < 0.6f) {
+            // use resistor 2
+            resistor2 = targetResistance;
+            resistor1 = DifferentFrom (targetResistance);
+        } else if (puzzle < 0.9f) {
+            // 2 resistors in serial
+            // TODO randomize this
+            resistor1 = targetResistance * 0.4;
+            resistor2 = targetResistance - resistor1;
+        } else {
+            // 2 resistors in parallel
+            // TODO randomize this
+            resistor1 = targetResistance * 1.5;
+            resistor2 = 1.0 / (1.0 / targetResistance - 1.0 / resistor1);
+        }
+        Debug.Log ("[Resistors] Top resistor is " + resistor1);
+        Debug.Log ("[Resistors] Bottom resistor is " + resistor2);
 
-		DisplayResistor (resistor1, 0, 1, 2, 3, 4);
-		DisplayResistor (resistor2, 5, 6, 7, 8, 9);
+        DisplayResistor (resistor1, 0, 1, 2, 3, 4);
+        DisplayResistor (resistor2, 5, 6, 7, 8, 9);
     }
 
-	Material GetBandMaterial (int index) {
-		switch (index) {
-		case -2:
-			return materialSilver;
-		case -1:
-			return materialGold;
-		case 0:
-			return materialBlack;
-		case 1:
-			return materialBrown;
-		case 2:
-			return materialRed;
-		case 3:
-			return materialOrange;
-		case 4:
-			return materialYellow;
-		case 5:
-			return materialGreen;
-		case 6:
-			return materialBlue;
-		case 7:
-			return materialViolet;
-		case 8:
-			return materialGray;
-		case 9:
-			return materialWhite;
-		default:
-			return null;
-		}
-	}
+    Material GetBandMaterial (int index) {
+        switch (index) {
+        case -2:
+            return materialSilver;
+        case -1:
+            return materialGold;
+        case 0:
+            return materialBlack;
+        case 1:
+            return materialBrown;
+        case 2:
+            return materialRed;
+        case 3:
+            return materialOrange;
+        case 4:
+            return materialYellow;
+        case 5:
+            return materialGreen;
+        case 6:
+            return materialBlue;
+        case 7:
+            return materialViolet;
+        case 8:
+            return materialGray;
+        case 9:
+            return materialWhite;
+        default:
+            return null;
+        }
+    }
 
-	void DisplayResistor (double resistanceValue, int i0, int i1, int i2, int i3, int i4) {
-		int multiplier, digit1, digit2;
-		if (resistanceValue < 10) {
-			multiplier = -1;
-		} else {
-			multiplier = 0;
-			while (true) {
-				if (resistanceValue < System.Math.Pow(10.0, multiplier + 2))
-					break;
-				multiplier++;
-			}
-		}
-		int display = (int) (resistanceValue / System.Math.Pow (10.0, multiplier));
-		digit1 = display / 10;
-		digit2 = display % 10;
+    void DisplayResistor (double resistanceValue, int i0, int i1, int i2, int i3, int i4) {
+        int multiplier, digit1, digit2;
+        if (resistanceValue < 10) {
+            multiplier = -1;
+        } else {
+            multiplier = 0;
+            while (true) {
+                if (resistanceValue < System.Math.Pow(10.0, multiplier + 2))
+                    break;
+                multiplier++;
+            }
+        }
+        int display = (int) (resistanceValue / System.Math.Pow (10.0, multiplier));
+        digit1 = display / 10;
+        digit2 = display % 10;
 
-		bool displayFlipped = Random.Range (0, 5) == 0;
-		if (displayFlipped) {
-			bands [i0].material = materialGray;
-			bands [i1].enabled = false;
-			bands [i2].material = GetBandMaterial (multiplier);
-			bands [i3].material = GetBandMaterial (digit2);
-			bands [i4].material = GetBandMaterial (digit1);
-		} else {
-			bands [i0].material = GetBandMaterial (digit1);
-			bands [i1].material = GetBandMaterial (digit2);
-			bands [i2].material = GetBandMaterial (multiplier);
-			bands [i3].enabled = false;
-			bands [i4].material = materialGray;
-		}
-	}
+        Material toleranceColor = materialGray;
+        switch (Random.Range(0, 6)) {
+            case 0:
+                toleranceColor = materialBrown;
+                break;
+            case 1:
+                toleranceColor = materialRed;
+                break;
+            case 2:
+                toleranceColor = materialGreen;
+                break;
+            case 3:
+                toleranceColor = materialBlue;
+                break;
+            case 4:
+                toleranceColor = materialViolet;
+                break;
+            case 5:
+                toleranceColor = materialGray;
+                break;
+        }
+
+        bool displayFlipped = Random.Range (0, 4) == 0;
+        if (displayFlipped) {
+            bands [i0].material = toleranceColor;
+            bands [i1].enabled = false;
+            bands [i2].material = GetBandMaterial (multiplier);
+            bands [i3].material = GetBandMaterial (digit2);
+            bands [i4].material = GetBandMaterial (digit1);
+        } else {
+            bands [i0].material = GetBandMaterial (digit1);
+            bands [i1].material = GetBandMaterial (digit2);
+            bands [i2].material = GetBandMaterial (multiplier);
+            bands [i3].enabled = false;
+            bands [i4].material = toleranceColor;
+        }
+    }
 
     void OnPress(int buttonNumber)
     {
@@ -306,5 +327,21 @@ public class ExampleModule : MonoBehaviour
         else if (point1 == 3 && point2 == 7) wireIndex = 27;
         
         wires [wireIndex].SetActive (isPresent);
+    }
+
+    void OnClear() {
+        GetComponent<KMAudio>().PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, transform);
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                SetWire(i, j, false);
+            }
+        }
+        startedConnecting = -1;
+    }
+
+    void OnCheck() {
+        GetComponent<KMAudio>().PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, transform);
+        // TODO
+        GetComponent<KMBombModule>().HandlePass();
     }
 }
